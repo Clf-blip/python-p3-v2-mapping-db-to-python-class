d113d97b27b3c69b4476e8e6b94227b716a17c68
jonathan <cliffjonathan66@gmail.com>
2025-05-30 15:53:14 +0300

    work

--- lib/department.py
+++ lib/department.py
@@ -1,6 +1,5 @@
 from __init__ import CURSOR, CONN
 
-
 class Department:
 
     def __init__(self, name, location, id=None):
@@ -13,7 +12,6 @@ class Department:
 
     @classmethod
     def create_table(cls):
-        """ Create a new table to persist the attributes of Department instances """
         sql = """
             CREATE TABLE IF NOT EXISTS departments (
             id INTEGER PRIMARY KEY,
@@ -25,7 +23,6 @@ class Department:
 
     @classmethod
     def drop_table(cls):
-        """ Drop the table that persists Department instances """
         sql = """
             DROP TABLE IF EXISTS departments;
         """
@@ -33,28 +30,21 @@ class Department:
         CONN.commit()
 
     def save(self):
-        """ Insert a new row with the name and location values of the current Department instance.
-        Update object id attribute using the primary key value of new row.
-        """
         sql = """
             INSERT INTO departments (name, location)
             VALUES (?, ?)
         """
-
         CURSOR.execute(sql, (self.name, self.location))
         CONN.commit()
-
         self.id = CURSOR.lastrowid
 
     @classmethod
     def create(cls, name, location):
-        """ Initialize a new Department instance and save the object to the database """
         department = cls(name, location)
         department.save()
         return department
 
     def update(self):
-        """Update the table row corresponding to the current Department instance."""
         sql = """
             UPDATE departments
             SET name = ?, location = ?
@@ -64,11 +54,41 @@ class Department:
         CONN.commit()
 
     def delete(self):
-        """Delete the table row corresponding to the current Department instance"""
         sql = """
             DELETE FROM departments
             WHERE id = ?
         """
-
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
+        self.id = None
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT * FROM departments WHERE id = ?
+        """
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        if row:
+            return cls.instance_from_db(row)
+        return None
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT * FROM departments WHERE name = ?
+        """
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        if row:
+            return cls.instance_from_db(row)
+        return None
+
+    @classmethod
+    def instance_from_db(cls, row):
+        id, name, location = row
+        return cls(name=name, location=location, id=id)
+
+    @classmethod
+    def get_all(cls):
+        sql = "SELECT * FROM departments"
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
